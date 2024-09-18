23939a5a72cb9889ddd085c02fd1af47ecad60ee
Sarthak-Srivastava-01 <sarthak.srivastava1398@gmail.com>
2024-09-18 10:57:52 +0530

    crud operations

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -8,14 +8,37 @@ const Model = require('../models/UserModel');
 // PUT - To update data
 // DELETE - To delete data
 
+// Response status codes
+// 1. 100 - 199 : Informational response status code
+// 1. 200 - 299 : Success status code
+// 1. 300 - 399 : Redirect status code
+// 1. 400 - 499 : Client side error
+// 1. 500 - 599 : Server side error
+
+
 // endpoint or route
 router.post('/add', (req, res) => {
     console.log(req.body);
-    res.send('Response from user add');
+
+    new Model(req.body).save()
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);        
+        res.status(500).json(err);
+    });
+    
+    // res.send('Response from user add');
 });
 
 router.get('/getall', (req, res) => {
-    res.send('response from user getall');
+    Model.find()
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);        
+        res.status(500).json(err);
+    });
 });
 
 router.get('/getbyid', (req, res) => {
