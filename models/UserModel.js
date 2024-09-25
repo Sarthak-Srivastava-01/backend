044e97d3d493a12f2c07ef11cff053280fe0bb86
Sarthak-Srivastava-01 <sarthak.srivastava1398@gmail.com>
2024-09-25 11:27:51 +0530

    added city in user db

--- models/UserModel.js
+++ models/UserModel.js
@@ -9,6 +9,7 @@ const mySchema = new Schema({
     name : {type : String, required : true},
     email : {type : String, unique : true},
     password : String,
+    city : {type : String, default : 'Unknown'},
     createdAt : {type : Date, default : Date.now}
 })
 
